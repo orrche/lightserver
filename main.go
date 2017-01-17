c6e6a9c7ca10a69f19396a5cac7bbfdf29a9c5c6
Kent Gustavsson <kent@minoris.se>
2017-01-18 00:32:35 +0100

    Switch to using camq
    Signed-off-by: Kent Gustavsson <kent@minoris.se>

--- main.go
+++ main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"log"
 	"log/syslog"
 	"os"
 	"os/exec"
 
+	"minoris.se/rabbitmq/camq"
+
 	"github.com/BurntSushi/toml"
-	"github.com/streadway/amqp"
 )
 
-type AMQConfig struct {
-	URL        string
-	Queue      string
-	ClientKey  string
-	CACert     string
-	ServerCert string
-}
-
 type Config struct {
-	AMQ      AMQConfig
+	AMQ      camq.AMQConfig
 	Commands []Command
 }
 
@@ -30,13 +21,6 @@ type Command struct {
 	ID  string
 	Cmd string
 }
-
-type Amq struct {
-	conn    *amqp.Connection
-	channel *amqp.Channel
-}
-
-var amq Amq
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -56,63 +40,37 @@ func ReadConfig() Config {
 
 	return config
 }
-
 func main() {
 	config := ReadConfig()
-
 	logwriter, err := syslog.New(syslog.LOG_NOTICE, "lightserver")
-	if err == nil {
+	if err != nil {
 		log.SetOutput(logwriter)
 	} else {
 		log.Print("Unable to connect to syslog", err)
 	}
 
-	cfg := new(tls.Config)
+	amq := camq.GetAMQChannel(config.AMQ)
+	amq.DeclareExchange()
 
-	cfg.RootCAs = x509.NewCertPool()
-	cfg.RootCAs.AppendCertsFromPEM([]byte(config.AMQ.CACert))
+	msgs, err := amq.GetExchangeChannel()
 
-	cert, _ := tls.X509KeyPair([]byte(config.AMQ.ServerCert), []byte(config.AMQ.ClientKey))
-	cfg.Certificates = append(cfg.Certificates, cert)
+	if err != nil {
+		log.Print(err)
+		return
+	}
 
-	cfg.BuildNameToCertificate()
-	conn, err := amqp.DialTLS(config.AMQ.URL, cfg)
-	amq.conn = conn
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer amq.conn.Close()
-
-	ch, err := amq.conn.Channel()
-	failOnError(err, "Failed to create channel")
-	amq.channel = ch
-	defer amq.channel.Close()
-
-	err = ch.ExchangeDeclare(config.AMQ.Queue, "fanout", false, false, false, false, nil)
-	failOnError(err, "Failed to declare exchange")
-
-	q, err := amq.channel.QueueDeclare("", false, false, true, false, nil)
-	failOnError(err, "Failed to declare a queue")
-	err = ch.QueueBind(q.Name, "", config.AMQ.Queue, false, nil)
-	failOnError(err, "Failed to bind a queue")
-
-	msgs, err := amq.channel.Consume(q.Name, "", true, false, false, false, nil)
-	failOnError(err, "Failed to register a consumer")
-
-	forever := make(chan bool)
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s\n", d.Body)
-			bdy := string(d.Body)
-			for _, cmd := range config.Commands {
-				if cmd.ID == bdy {
-					go func(cmd Command) {
-						c := exec.Command("bash", "-c", cmd.Cmd)
-						c.Start()
-						err = c.Wait()
-						log.Printf("Command %s:%s finished with error: %v", cmd.ID, cmd.Cmd, err)
-					}(cmd)
-				}
+	for d := range msgs {
+		log.Printf("Received a message: %s\n", d.Body)
+		bdy := string(d.Body)
+		for _, cmd := range config.Commands {
+			if cmd.ID == bdy {
+				go func(cmd Command) {
+					c := exec.Command("bash", "-c", cmd.Cmd)
+					c.Start()
+					err = c.Wait()
+					log.Printf("Command %s:%s finished with error: %v", cmd.ID, cmd.Cmd, err)
+				}(cmd)
 			}
 		}
-	}()
-	<-forever
+	}
 }
